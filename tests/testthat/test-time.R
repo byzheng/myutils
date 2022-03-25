2a2971b46849b1dbd9e9c06ea66378cd68809047
Bangyou Zheng <zhe00a@sc-55-cdc.it.csiro.au>
2022-03-25 16:00:35 +1100

    update test

--- tests/testthat/test-time.R
+++ tests/testthat/test-time.R
@@ -1,4 +1,4 @@
-test_that("Statistics functions", {
+test_that("Test time", {
     first_day <- month_first_day()
     expect_equal(doy2date(first_day)[1:2],
                  c("01-Jan", "01-Feb"))
