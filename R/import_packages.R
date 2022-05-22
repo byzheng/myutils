ff61f5128ae3a1bb1f44495dc46b15dc2d9743c8
Bangyou Zheng <bangyou.zheng@csiro.au>
2022-05-23 09:51:30 +1000

    Fixed errors for check

--- R/import_packages.R
+++ R/import_packages.R
@@ -2,4 +2,17 @@
 #' @importFrom magrittr %>%
 #' @importFrom snow setDefaultClusterOptions
 #' @importFrom rlang .data
+#' @importFrom expDB expdbConnect
+#' @importFrom htcondor ncOpen
+#' @importFrom parallel makeCluster
+#' @importFrom rapsimng get_metfile
+#' @importFrom rnaturalearth ne_coastline
+#' @importFrom rproject project_fun
+#' @importFrom tidyverse tidyverse_conflicts
+#' @importFrom weaana readWeatherRecords
 NULL
+
+
+.import_extra <- function() {
+    a <- rnaturalearthdata::coastline110
+}
