77b92798f73866238ece544a2d69bd072240b003
Bangyou Zheng @ RStudio Server <bangyou.zheng@csiro.au>
2024-02-21 23:17:34 +1100

    import utils

--- R/rpackage.R
+++ R/rpackage.R
@@ -5,11 +5,11 @@
 #' @export
 install_packages <- function() {
     if (!require("pak")) {
-        install.packages("pak")
+        utils::install.packages("pak")
     }
     pkgs <- c("oz", "tidyverse", "RMySQL")
     i <- 3
     for (i in seq(along = pkgs)) {
-        install.packages(pkgs[i])
+        utils::install.packages(pkgs[i])
     }
 }
