d608e7e365794359d6e669e60740aa76561bf142
Bangyou Zheng @ RStudio Server <bangyou.zheng@csiro.au>
2024-02-26 13:03:56 +1100

    Remove unused dependence to faster testing

--- R/import_packages.R
+++ R/import_packages.R
@@ -2,13 +2,6 @@
 #' @importFrom magrittr %>%
 #' @importFrom snow setDefaultClusterOptions
 #' @importFrom rlang .data
-#' @importFrom expDB expdbConnect
 #' @importFrom parallel makeCluster
-#' @importFrom rapsimng get_metfile
-#' @importFrom rproject project_fun
-#' @importFrom weaana readWeatherRecords
-#' @importFrom rtiddlywiki tiddler_document
-#' @importFrom BWGS bwgs.predict
-#' @importFrom taskqueue db_connect
 NULL
 
--- R/rpackage.R
+++ R/rpackage.R
@@ -4,9 +4,19 @@
 #' @return no return
 #' @export
 install_packages <- function() {
-    pkgs <- c("oz", "tidyverse", "RMySQL")
+    pkgs <- c("oz", "tidyverse", "RMySQL", "remotes")
     i <- 3
     for (i in seq(along = pkgs)) {
         utils::install.packages(pkgs[i])
     }
+
+    # github packages
+    g_packages <- c("byzheng/weaana", "byzheng/rproject",
+      "byzheng/rtiddlywiki", "byzheng/rapsimng",
+      "byzheng/BWGS", "byzheng/taskqueue")
+
+    for (i in seq(along = g_packages)) {
+        remotes::install_github(g_packages[i], force = "TRUE")
+    }
+
 }
