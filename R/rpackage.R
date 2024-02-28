de1b9b027afb13423aba715fc785cabe3561f7f1
Bangyou Zheng @ RStudio Server <bangyou.zheng@csiro.au>
2024-02-29 00:40:41 +1100

    Update package

--- R/rpackage.R
+++ R/rpackage.R
@@ -4,21 +4,41 @@
 #' @return no return
 #' @export
 install_packages <- function() {
+
+    # Update existing packages
+    if (!require("pak")) {
+        utils::install.packages("pak")
+    }
     # CRAN packages
     pkgs <- c("oz", "tidyverse", "RMySQL", "remotes", "roxygen2", "arrow",
-              "RColorBrewer")
-    i <- 3
+              "RColorBrewer",
+              "byzheng/weaana", "byzheng/rproject",
+              "byzheng/rtiddlywiki", "byzheng/rapsimng",
+              "byzheng/BWGS", "byzheng/taskqueue")
+
     for (i in seq(along = pkgs)) {
-        utils::install.packages(pkgs[i])
+        if (.Platform$OS.type == "linux") {
+            dps <- pak::pkg_deps(pkgs[i])
+            for (j in seq(along = dps[[1]])) {
+                pak::pkg_install(dps$package[j], upgrade = TRUE, ask = FALSE)
+            }
+        }
+        pak::pkg_install(pkgs[i], upgrade = TRUE, ask = FALSE)
+        #
+        # if (!require(pkgs[i], character.only = TRUE, quietly = TRUE)) {
+        #     utils::install.packages(pkgs[i])
+        # }
     }
-
-    # github packages
-    g_packages <- c("byzheng/weaana", "byzheng/rproject",
-      "byzheng/rtiddlywiki", "byzheng/rapsimng",
-      "byzheng/BWGS", "byzheng/taskqueue")
-
-    for (i in seq(along = g_packages)) {
-        remotes::install_github(g_packages[i], force = "TRUE")
-    }
+#
+#     # github packages
+#     g_packages <- c("byzheng/weaana", "byzheng/rproject",
+#       "byzheng/rtiddlywiki", "byzheng/rapsimng",
+#       "byzheng/BWGS", "byzheng/taskqueue")
+#
+#     for (i in seq(along = g_packages)) {
+#         if (!require(pkgs[i], character.only = TRUE, quietly = TRUE)) {
+#             remotes::install_github(g_packages[i], force = "TRUE")
+#         }
+#     }
 
 }
