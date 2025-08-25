5e0de57490cc50e6d768e456320bf4f25d6d8f60
Bangyou Zheng <zheng.bangyou@gmail.com>
2025-08-25 14:44:15 +1000

    Restore the testing code

--- R/rpackage.R
+++ R/rpackage.R
@@ -18,6 +18,45 @@ pkgs <- c("oz",
             "byzheng/ragridata",
             "byzheng/twproject")
 
+.load_modules <- function() {
+    if (.Platform$OS.type != "unix") {
+        return(invisible(NULL))
+    }
+    module_check <- suppressWarnings(system("command -v module", 
+            intern = TRUE, ignore.stderr = TRUE))
+    if (!nzchar(module_check)) {
+        return(invisible(NULL))
+    }
+    
+    for (i in seq(along = modules)) {
+        # Find all available versions for the module
+        avail_cmd <- paste("module avail", modules[i])
+        avail_out <- suppressWarnings(system(avail_cmd, intern = TRUE))
+        avail_out <- avail_out[grepl(modules[i], avail_out)]
+        if (length(avail_out) == 0) {
+            next
+        }
+        # Extract versions and pick the highest one
+        versions <- regmatches(avail_out, gregexpr(paste0(modules[i], "/[0-9.]+"), avail_out))
+        versions <- unlist(versions)
+        if (length(versions) == 0) {
+            next
+        }
+        # Sort versions and pick the highest
+        ver_nums <- sub(paste0(modules[i], "/"), "", versions)
+        highest <- versions[order(package_version(ver_nums), decreasing = TRUE)][1]
+        message("Loading module: ", highest)
+        cmd <- sprintf("bash -l -c 'module load %s; env'", highest)
+        env_out <- tryCatch(system(cmd, intern = TRUE), error = function(e) character())
+        if (length(env_out) == 0) next
+        env_vars <- strsplit(env_out, "=", fixed = TRUE)
+        for (kv in env_vars) {
+            if (length(kv) != 2) next
+            do.call(Sys.setenv, setNames(list(kv[2]), kv[1]))
+        }
+    }
+    tryCatch(system("bash -l -c 'module list'"), error = function(e) NULL)
+}
 
 #' Install packages for myself
 #'
@@ -29,7 +68,7 @@ install_packages <- function() {
     if (!base::require("pak", quietly = TRUE, warn.conflicts = FALSE)) {
         utils::install.packages("pak")
     }
-    #.load_modules()
+    # .load_modules()
     # CRAN packages
     i <- 1
     for (i in seq(along = pkgs)) {
